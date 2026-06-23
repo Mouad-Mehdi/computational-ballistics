a89de06c0d8bbd7144cc0b1b1ffd0305da6b7f7d
Mouad-Mehdi <mouadmehdi2006@gmail.com>
2026-06-23 14:19:43 +0100

    implemented Euler's method to solve ODEs

--- utils/utilities.py
+++ utils/utilities.py
@@ -11,3 +11,25 @@ def plot_trajectory(X, Y, x0 ,y0, title):
     plt.title(title)
     plt.legend()
     plt.show()
+
+# Implementing Euler's aproximation to get a set of points representing V([a,b]) 
+# given a certain ODE dV/dt = f(V), a step size 'h' , and an initial condition V(0) = z0  :
+def euler(f, a, b, h, z0):
+    t = a
+    z = z0
+    points = []
+    while t < b :
+        points.append(z)
+        z = z + h*f(z)
+        t = t + h
+    return np.array(points)
+
+# Implementing Euler's approximation to compute the integral of a function
+# given the derivative values, a step size h (the same one used to obtain
+# the sample), and an initial value z0.
+def euler_integral(derivatives, h, z0):
+    z = z0
+    points = []
+    for i in derivatives:
+        points.append(z)
+        z = z +h*i
