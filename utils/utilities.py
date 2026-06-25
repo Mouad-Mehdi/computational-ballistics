a3c0f6aca201fb926f5cebc375ba41800c3f8c8a
Mouad-Mehdi <mouadmehdi2006@gmail.com>
2026-06-25 18:26:01 +0100

    merge the euler implementations into a single function handling state vector and added an RK4 implementation.

--- utils/utilities.py
+++ utils/utilities.py
@@ -13,57 +13,44 @@ def plot_trajectory(X, Y, x0 ,y0, title):
     plt.legend()
     plt.show()
 
-# Function that plots the bullet trajectory given the initial conditions, and compares it with the Euler's approximation of the trajectory
-def plot_trajectory_comp(X, Y, X_euler, Y_euler, x0 ,y0, title):
+# Function that plots the bullet trajectory given the initial conditions,
+# and compares it with another trajectory
+def plot_trajectory_comp(X1, Y1, X2, Y2, x0 ,y0, title, plot1, plot2):
     plt.grid(True)
     plt.xlabel("Distance (m)")
     plt.ylabel("Height (m)")
     plt.axis('equal')
     plt.scatter(x0, y0, color="red", label="Launch point")
-    plt.plot(X,Y,color="blue",label="Trajectory")
-    plt.plot(X_euler,Y_euler,color="green",label="Euler's approximation")
+    plt.plot(X1,Y1,color="blue",label=plot1)
+    plt.plot(X2,Y2,color="orange",label=plot2)
     plt.title(title)
     plt.legend()
     plt.show()
 
-# Implementing Euler's aproximation to get a set of points representing V([a,b]) 
-# given a certain ODE dV/dt = f(V), a step size 'h' , and an initial condition V(0) = z0  :
-def euler(f, a, b, h, initial):
-    t = a
-    value = initial
-    points = []
-    while t < b :
-        points.append(value)
-        value = value + h*f(value)
-        t = t + h
-    return np.array(points)
 
-# Implementing Euler's approximation to compute the integral of a function
-# given the derivative values, a step size h (the same one used to obtain
-# the sample), and an initial value z0.
-def euler_integral(derivatives, h, initial):
-    value = initial
-    points = []
-    for derivative in derivatives:
-        points.append(value)
-        value = value +h*derivative
-    return np.array(points)
-
-# a function used to get sample points for x(t)/y(t) given an ODE dV/dt = f(V),
-# two initial conditions v0 and z0, and a step size h.
-def position_from_velocity_ode(f, a, b, h, initial_v, initial_pos):
-    Velocity = euler(f, a, b, h, initial_v)
-    positions = euler_integral(Velocity, h, initial_pos)
-    return positions
-
-# Redifining a new function implementing Euler's method with a a function taking 
+# Redifining a new function implementing Euler's method with a function taking 
 # the state vector instead of an integer as parameter.
-def euler_vectorial(f, a, b, h, initial):
+def euler(f, a, b, h, initial):
     t = a
     state = initial
     points = []
     while t < b :
         points.append(state)
         state = state + h*f(state)
+        t = t + h
+    return np.array(points)
+
+# An implementation of RK4 :
+def rk4(f,a,b,h,initial):
+    t = a
+    state = initial
+    points = []
+    while t < b :
+        points.append(state)
+        k1 = f(state)
+        k2 = f(state + (h/2) * k1)
+        k3 = f(state + (h/2) * k2)
+        k4 = f(state + h*k3)
+        state = state + (h/6)*(k1 +2*k2 + 2*k3 + k4)
         t = t + h
     return np.array(points)
